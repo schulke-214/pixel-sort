ada793d2a0b5d65939de35307ff046adf35a6d24
schulke-214 <schulkemaximilian@gmail.com>
2019-02-26 21:38:42 +0100

    fix(lightsort): implement the code example of the docs

--- test/lightsort.js
+++ test/lightsort.js
@@ -1,10 +1,33 @@
-const { Sorter } = require('../lib/index.js');
+const {
+    Sorter
+} = require('../lib/index.js');
 
 let sorter = new Sorter();
 
-( async () =>  {
+void async function () {
     await sorter.load('./data/example.jpg');
-    await sorter.lightsort({ direction: 'LTR' }, () => console.log("callback"));
-    await sorter.lightsort({ direction: 'BTT' });
+
+    await sorter.lightsort({
+        direction: 'LTR'
+    }, () => console.log("sorted"));
+
+    // await sorter.lightsort({
+    //     direction: 'BTT'
+    // });
+
     await sorter.save('./output/lightsort.png');
-} )()
+}()
+
+// sorter.load('./data/example.jpg', err => {
+//     if (err) throw err;
+
+//     sorter.lightsort({
+//         direction: 'LTR'
+//     }, err => {
+//         if (err) throw err;
+
+//         sorter.save('./output/lightsort.png', err => {
+//             if (err) throw err;
+//         });
+//     });
+// });
